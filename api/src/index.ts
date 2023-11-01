b5825ea6131dca3e1ed86f1b1531c0836923fcee
Ayush Kumar <98096047+Ayush211107@users.noreply.github.com>
2023-11-02 00:58:44 +0530

    Add messMenu route

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import attendenceRoutes from "./routes/attendence.route";
+import menuRoutes from "./routes/messMenu.route";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/attendence", attendenceRoutes);
+app.use("/api/menu", menuRoutes);
 
 app.listen(port, () => {
 	console.log(`Server listening at http://localhost:${port}`);
