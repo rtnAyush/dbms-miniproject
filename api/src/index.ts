7bd54aca40e538a221d6787d54f2d2d90147dbb3
Ayush Kumar <98096047+Ayush211107@users.noreply.github.com>
2023-11-01 21:55:15 +0530

    Ayush (#14) fix routes dir
    * created routes dir
    
    * add route
    
    * setted route dir

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
 
+import routes from "./routes/attendence.route";
+
+
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 
-import routes from "./routes";
-app.use(routes);
+app.use("/api", routes);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
