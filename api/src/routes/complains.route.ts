2da44a01d36c8d37ccebbb5d254ca44350393397
Ayush Kumar <98096047+Ayush211107@users.noreply.github.com>
2023-11-02 19:27:13 +0530

    imporove and test all routes (#38)
    * add attendence route
    
    * add attendence route
    
    * Add messMenu route
    
    * fix and tested attendence route
    
    * fix type issue
    
    * improve and test all routes

--- api/src/routes/complains.route.ts
+++ api/src/routes/complains.route.ts
@@ -4,127 +4,156 @@ import prisma from "../utils/prisma";
 const routes = Router();
 
 routes
-  .route("/")
-  .get(async (req: Request, res: Response) => {
-    let filter = {};
-    let sortOptions = {};
-    const { userId1, date, upvote, downvote } = req.body;
-	let userId = parseInt(userId1)
-    try {
-      if (userId) {
-        filter["id"] = userId;
-      }
-      if (date) {
-        filter["createdAt"] = date;
-      }
+	.route("/")
+	.get(async (req: Request, res: Response) => {
+		let filter = {};
+		let sortOptions = {};
+		const { userId, date, upvote, downvote } = req.body;
 
-      if (upvote && downvote) {
-        throw new Error("Application of multiple sorting options.");
-      }
-      if (upvote) {
-        sortOptions["upvote"] = "desc";
-      }
-      if (downvote) {
-        sortOptions["downvote"] = "desc";
-      }
+		try {
+			if (userId) {
+				filter["id"] = userId;
+			}
+			if (date) {
+				filter["createdAt"] = date;
+			}
 
-      const complains = await prisma.complains.findMany({
-        where: filter,
-        select: {
-          title: true,
-          description: true,
-        },
-        orderBy: sortOptions,
-      });
+			if (upvote && downvote) {
+				throw new Error("Application of multiple sorting options.");
+			}
+			if (upvote) {
+				sortOptions["upvote"] = "desc";
+			}
+			if (downvote) {
+				sortOptions["downvote"] = "desc";
+			}
 
-      return res
-        .status(200)
-        .json({ error: false, msg: "Success", data: complains });
-    } catch (err: any) {
-      return res.status(400).json({ error: true, msg: err?.message });
-    }
-  })
-  .post(async (req: Request, res: Response) => {
-    const { userId1, title, desc, session } = req.body;
-	let userId = parseInt(userId1)
-    const newComplain = await prisma.complains.create({
-      data: {
-        title: title,
-        description: desc,
-        session: session,
-        authorId: userId,
-      },
-    });
-  })
-  .put(async (req: Request, res: Response) => {
-    const { vote, userId1, complainId1 } = req.body;
-	let userId = parseInt(userId1)
-	let complainId = parseInt(complainId1)
+			const complains = await prisma.complains.findMany({
+				where: filter,
+				select: {
+					title: true,
+					description: true,
+				},
+				orderBy: sortOptions,
+			});
 
-    const voterUpdate = await prisma.voteCalc.create({
-      data: {
-        voterId: userId,
-        complainId: complainId,
-        vote: vote,
-      },
-    });
+			return res
+				.status(200)
+				.json({ error: false, msg: "Success", data: complains });
+		} catch (err: any) {
+			return res.status(400).json({ error: true, msg: err?.message });
+		}
+	})
+	.post(async (req: Request, res: Response) => {
+		const { userId, title, desc, session } = req.body;
 
-    let updateVal = {};
-    if (vote == "up") {
-      const vo = await prisma.complains.findUnique({
-        where: {
-          id: complainId,
-        },
-        select: {
-          upvote: true,
-        },
-      });
-      updateVal["upvote"] = vo.upvote + 1;
-    } else if (vote == "down") {
-      const vo = await prisma.complains.findUnique({
-        where: {
-          id: complainId,
-        },
-        select: {
-          downvote: true,
-        },
-      });
-      updateVal["downvote"] = vo.downvote + 1;
-    }
+		try {
+			if (!userId) throw new Error("Missing userId");
+			if (!title) throw new Error("Missing title");
+			if (!desc) throw new Error("Missing desc");
+			if (!session) throw new Error("Missing session");
 
-    const voteAdd = await prisma.complains.update({
-      where: {
-        id: complainId,
-      },
-      data: updateVal,
-    });
+			const newComplain = await prisma.complains.create({
+				data: {
+					title: title,
+					description: desc,
+					session: session,
+					authorId: parseInt(userId),
+				},
+			});
+			return res
+				.status(200)
+				.json({ error: false, msg: "Success", data: newComplain });
+		} catch (err: any) {
+			return res.status(400).json({ error: true, msg: err?.message });
+		}
+	})
+	.put(async (req: Request, res: Response) => {
+		const { vote, userId, complainId } = req.body;
 
-    return res
-      .status(200)
-      .json({ error: false, msg: "Success", data: updateVal });
-  })
-  .delete(async (req: Request, res: Response) => {
-    const { complainId1 } = req.params;
-	let complainId = parseInt(complainId1)
+		try {
+			if (!vote) throw new Error("Missing vote");
+			if (!userId) throw new Error("Missing userId");
+			if (!complainId) throw new Error("Missing complainId");
 
-    const findComplain = await prisma.complains.findUnique({
-      where: { id: complainId },
-    });
+			const voterUpdate = await prisma.voteCalc.create({
+				data: {
+					voterId: userId,
+					complainId: complainId,
+					vote: vote,
+				},
+			});
 
-    const updateInDeletedComplain = await prisma.complainsDeleted.create({
-      data: {
-        createdAt: findComplain.createdAt,
-        session: findComplain.session,
-        title: findComplain.title,
-        description: findComplain.description,
-        upvote: findComplain.upvote,
-        downvote: findComplain.downvote,
-      },
-    });
+			let updateVal = {};
+			if (vote == "up") {
+				const vo = await prisma.complains.findUnique({
+					where: {
+						id: complainId,
+					},
+					select: {
+						upvote: true,
+					},
+				});
+				updateVal["upvote"] = vo.upvote + 1;
+			} else if (vote == "down") {
+				const vo = await prisma.complains.findUnique({
+					where: {
+						id: complainId,
+					},
+					select: {
+						downvote: true,
+					},
+				});
+				updateVal["downvote"] = vo.downvote + 1;
+			}
 
-    const deleteFromComplain = await prisma.complains.delete({
-      where: { id: complainId },
-    });
-  });
+			const voteAdd = await prisma.complains.update({
+				where: {
+					id: complainId,
+				},
+				data: updateVal,
+			});
+
+			return res
+				.status(200)
+				.json({ error: false, msg: "Success", data: updateVal });
+		} catch (err: any) {
+			return res.status(400).json({ error: true, msg: err?.message });
+		}
+	})
+	.delete(async (req: Request, res: Response) => {
+		const { complainId } = req.params;
+
+		try {
+			const findComplain = await prisma.complains.findUnique({
+				where: { id: parseInt(complainId) },
+			});
+
+			const updateInDeletedComplain =
+				await prisma.complainsDeleted.create({
+					data: {
+						createdAt: findComplain.createdAt,
+						session: findComplain.session,
+						title: findComplain.title,
+						description: findComplain.description,
+						upvote: findComplain.upvote,
+						downvote: findComplain.downvote,
+					},
+				});
+
+			const deleteFromComplain = await prisma.complains.delete({
+				where: { id: parseInt(complainId) },
+			});
+
+			return res.status(200).json({
+				error: false,
+				msg: "Success",
+				data: deleteFromComplain,
+			});
+		} catch (err: any) {
+			return res.status(400).json({ error: true, msg: err?.message });
+		}
+	});
+
 
 export default routes;
