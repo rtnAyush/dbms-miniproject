3eb176e1114ea49b7cc54cd275d53a1cc20c53c5
Ayush Kumar <98096047+Ayush211107@users.noreply.github.com>
2023-11-02 21:35:23 +0530

    improve api (#42)
    * add attendence route
    
    * add attendence route
    
    * Add messMenu route
    
    * fix and tested attendence route
    
    * fix type issue
    
    * improve and test all routes
    
    * add api docs

--- api/src/routes/messMenu.route.ts
+++ api/src/routes/messMenu.route.ts
@@ -5,108 +5,108 @@ import { Sessions } from "@prisma/client";
 const routes = Router();
 
 routes
-  .route("/")
-  .get(async (req: Request, res: Response) => {
-    const { day, session } = req.query;
+	.route("/")
+	.get(async (req: Request, res: Response) => {
+		const { day, session } = req.query;
 
-    const query = {};
+		const query = {};
 
-    if (day) {
-      query["day"] = day;
-    }
-    if (session) {
-      query["session"] = session as Sessions;
-    }
+		if (day) {
+			query["day"] = day;
+		}
+		if (session) {
+			query["session"] = session as Sessions;
+		}
 
-    try {
-      const menuToday = await prisma.menu.findMany({
-        where: query,
-        include: {
-          food: true,
-        },
-      });
+		try {
+			const menuToday = await prisma.menu.findMany({
+				where: query,
+				include: {
+					food: true,
+				},
+			});
 
-      return res
-        .status(200)
-        .json({ error: false, msg: "Success", data: menuToday });
-    } catch (err: any) {
-      return res.status(400).json({ error: true, msg: err?.message });
-    }
-  })
-  .post(async (req: Request, res: Response) => {
-    const { day, session, name } = req.body;
+			return res
+				.status(200)
+				.json({ error: false, msg: "Success", data: menuToday });
+		} catch (err: any) {
+			return res.status(400).json({ error: true, msg: err?.message });
+		}
+	})
+	.post(async (req: Request, res: Response) => {
+		const { day, session, name } = req.body;
 
-    try {
-      if (!day) throw new Error("Missing Day");
-      if (!name) throw new Error("Missing Name");
-      if (!session) throw new Error("Missing Session");
+		try {
+			if (!day) throw new Error("Missing Day");
+			if (!name) throw new Error("Missing Name");
+			if (!session) throw new Error("Missing Session");
 
-      const foodExists = await prisma.foodItems.findUnique({
-        where: { name: name },
-        select: { id: true },
-      });
+			const foodExists = await prisma.foodItems.findUnique({
+				where: { name: name },
+				select: { id: true },
+			});
 
-      if (foodExists == null) {
-        const foodId = await prisma.foodItems.create({
-          data: {
-            name: name,
-          },
-          select: { id: true },
-        });
+			if (foodExists == null) {
+				const foodId = await prisma.foodItems.create({
+					data: {
+						name: name,
+					},
+					select: { id: true },
+				});
 
-        const addMenu = await prisma.menu.create({
-          data: {
-            day: day,
-            session: session,
-            foodId: foodId.id,
-          },
-        });
-      } else {
-        const addMenu = await prisma.menu.create({
-          data: {
-            day: day,
-            session: session,
-            foodId: foodExists.id,
-          },
-        });
-      }
+				const addMenu = await prisma.menu.create({
+					data: {
+						day: day,
+						session: session,
+						foodId: foodId.id,
+					},
+				});
+			} else {
+				const addMenu = await prisma.menu.create({
+					data: {
+						day: day,
+						session: session,
+						foodId: foodExists.id,
+					},
+				});
+			}
 
-      return res.status(200).json({ error: false, msg: "Success" });
-    } catch (err: any) {
-      return res.status(400).json({ error: true, msg: err?.message });
-    }
-  })
-  .delete(async (req: Request, res: Response) => {
-    const { day, session, name } = req.params;
-    try {
-      if (!day) throw new Error("Missing Day");
-      if (!name) throw new Error("Missing Name");
-      if (!session) throw new Error("Missing Session");
+			return res.status(200).json({ error: false, msg: "Success" });
+		} catch (err: any) {
+			return res.status(400).json({ error: true, msg: err?.message });
+		}
+	})
+	.delete(async (req: Request, res: Response) => {
+		const { day, session, name } = req.params;
+		try {
+			if (!day) throw new Error("Missing Day");
+			if (!name) throw new Error("Missing Name");
+			if (!session) throw new Error("Missing Session");
 
-      const foodExists = await prisma.foodItems.findUnique({
-        where: { name: name },
-      });
+			const foodExists = await prisma.foodItems.findUnique({
+				where: { name: name },
+			});
 
-      if (foodExists.id == null) {
-        throw new Error("No record with such name.");
-      }
+			if (foodExists.id == null) {
+				throw new Error("No record with such name.");
+			}
 
-      const addToDeleted = await prisma.foodItemsDeleted.create({
-        data: {
-          name: foodExists.name,
-        },
-      });
+			const addToDeleted = await prisma.foodItemsDeleted.create({
+				data: {
+					name: foodExists.name,
+				},
+			});
 
-      const removeFromFoodItems = await prisma.foodItems.delete({
-        where: { id: foodExists.id },
-      });
+			const removeFromFoodItems = await prisma.foodItems.delete({
+				where: { id: foodExists.id },
+			});
 
-      return res
-        .status(200)
-        .json({ error: false, msg: "Success", data: foodExists });
-    } catch (err: any) {
-      return res.status(400).json({ error: true, msg: err?.message });
-    }
-  });
+			return res
+				.status(200)
+				.json({ error: false, msg: "Success", data: foodExists });
+		} catch (err: any) {
+			return res.status(400).json({ error: true, msg: err?.message });
+		}
+	});
 
 export default routes;
