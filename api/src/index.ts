874d6258d459602241d1b47c5a3e8d32347a2d3d
Ayush Kumar <98096047+Ayush211107@users.noreply.github.com>
2023-11-02 12:16:26 +0530

    fix and tested attendence route

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,15 +3,18 @@ import cors from "cors";
 
 import attendenceRoutes from "./routes/attendence.route";
 import menuRoutes from "./routes/messMenu.route";
+import userRoutes from "./routes/user.route";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api/attendence", attendenceRoutes);
 app.use("/api/menu", menuRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => {
 	console.log(`Server listening at http://localhost:${port}`);
